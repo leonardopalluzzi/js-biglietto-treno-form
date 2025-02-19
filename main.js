7dd61c434a6d441e1efa617d8f777a91dcf58894
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-19 18:05:10 +0100

    add age select

--- main.js
+++ main.js
@@ -3,21 +3,22 @@ const distanceEl = document.getElementById('distance');
 const ageEl = document.getElementById('age');
 
 
-
 function priceCalc (km, age) {
     const basePrice = km * 0.21;
     let finalPrice;
-    if (isNaN(km) || isNaN(age)) {
+    if (isNaN(km)) {
         alert('per favore inserire un numero');
         location.reload();
-    } else if (age >= 65) {
+    } else if (age == '65') {
         finalPrice = basePrice - ((basePrice * 40) / 100);
-    } else if (age < 18) {
+    } else if (age == 'minor') {
         finalPrice = basePrice - ((basePrice*20) / 100);
     } else {
         finalPrice = basePrice
     }
     return finalPrice;
+    
+    
 }
 
 formEl.addEventListener('submit', (e) => {
