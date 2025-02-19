a2718418ac48909585952684da0d07600b8f8721
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-19 19:35:50 +0100

    add on page result print

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const formEl = document.querySelector('.ticket-form');
 const distanceEl = document.getElementById('distance');
 const ageEl = document.getElementById('age');
+const nameEl = document.getElementById('name');
+
+const outName = document.querySelector('.out-name');
+const outAge = document.querySelector('.out-age');
+const outOffer = document.querySelector('.out-offer');
+const outTrain = document.querySelector('.out-train');
+const outCode = document.querySelector('.out-code');
+const outPrice = document.querySelector('.out-price');
+
+const offers = ['Minorenni', 'Over 65', 'Nessun Offerta'];
+let i;
 
 
 function priceCalc (km, age) {
@@ -11,18 +22,32 @@ function priceCalc (km, age) {
         location.reload();
     } else if (age == '65') {
         finalPrice = basePrice - ((basePrice * 40) / 100);
+        i = 1;
     } else if (age == 'minor') {
         finalPrice = basePrice - ((basePrice*20) / 100);
+        i = 0;
     } else {
         finalPrice = basePrice
+        i = 2;
     }
     return finalPrice.toFixed(2);
+}
+
+function ticketGenerator () {
+    outName.innerHTML = nameEl.value;
+    outAge.innerHTML = ageEl.value;
+    outOffer.innerHTML = offers[i];
+    outTrain.innerHTML = Math.floor(Math.random() * 12);
+    outCode.innerHTML = Math.floor(Math.random() * 100000);
+    outPrice.innerHTML = priceCalc(distanceEl.value, ageEl.value);
     
-    
+
 }
 
 formEl.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(distanceEl, ageEl);
     console.log(priceCalc(distanceEl.value, ageEl.value));
+    console.log(ticketGenerator());
+    
 });
